f594929bf5d5c2776a1df27e8825d49b85129d5c
kirill.qa <kirill.qa@megapartners.io>
2023-06-05 00:45:07 +0200

    add first test - get single user

---
+++ tests/apiTests.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test.describe.parallel('Api testing', () => {
+  const BASE_URL = 'https://reqres.in/api'
+
+  //GET
+  test('Api test - assert response status get single user', async ({ request }) => {
+    const response = await request.get(`${BASE_URL}/users/2`);
+    const responseBody = JSON.parse(await response.text());
+
+    expect(response.status()).toBe(200);
+    expect(responseBody.data).toBeTruthy();
+    expect(responseBody.support).toBeTruthy();
+    expect(responseBody.data.id).toBeTruthy();
+  }) 
+});
